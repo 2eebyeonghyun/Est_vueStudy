6ce14f2ceafc31148510b741ff1e14b8df3f1e92
2eebyeonghyun <wada0012@naver.com>
2025-01-15 15:57:21 +0900

    25.01.15 studuy

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+    // export default {
+    //     data() {
+    //         return {
+    //             message: 'oh! this is vue?'
+    //         };
+    //     }
+    // };
+</script>
+
+<template>
+    <!-- 다른 컴포넌트들을 붙여오겠음 -->
+    <h1>음식들</h1>
+    <item-list/>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/itemList.vue
@@ -0,0 +1,24 @@
+<!-- 
+    머리 몸통 다리로 구분하듯이
+    template script style로 둘 것
+-->
+
+<template>
+    <div>
+        <h2>{{ name }}</h2>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: '사과',
+                message: '사과가 좋습니다.'
+            }
+        }
+    };
+</script>
+
+<style></style>
